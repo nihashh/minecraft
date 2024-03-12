eb140cceb7075fb10966317c6d20de8a73f736f9
nihashh <162719080+nihashh@users.noreply.github.com>
2024-03-12 11:23:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://p.turbosquid.com/ts-thumb/D0/a5ersl/Xc/1bee/jpg/1642611795/600x600/fit_q87/589da16c11a01c1331beea70e8a9655caa07d119/1bee.jpg" ;
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://pxcafe.com.br/wp-content/uploads/2023/10/Minecraft.jpg" ;
+}
